c45a0e698d64c5a8c30085789b1938a0e8ea8a5a
Sage Beard <sagek711@gmail.com>
2017-08-10 21:30:18 -0700

    initialize issue

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,15 @@
-require "kele/version"
+require 'httparty'
 
-module Kele
-  "Hello, World!"
+class Kele
 
-end
+  def initialize(email, password)
+  end
+  # include HTTParty
+  # base_uri 'https://www.bloc.io/api/v1'
+
+  # response = self.class.post('/sessions', body: { email: email, password: password } )
+  #   @auth_token = response['auth_token']
+  #   if @auth_token.nil?
+  #     puts "Sorry, invalid credentials."
+  #   end
+  # end
