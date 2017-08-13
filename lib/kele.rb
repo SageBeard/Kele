b120efd321f19de8ec91f5fba3dd6a4cb0011fdb
Sage Beard <sagek711@gmail.com>
2017-08-13 16:58:58 -0700

    initialize

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,15 +1,18 @@
 require 'httparty'
+require 'json'
+
 
 class Kele
+  include HTTParty
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
+    options = {
+      body: { email: email, password: password } }
+    response = self.class.post('/sessions', options)
+    @auth_token = response['auth_token']
+    if @auth_token.nil?
+      puts "Sorry, invalid credentials."
+    end
   end
-  # include HTTParty
-  # base_uri 'https://www.bloc.io/api/v1'
-
-  # response = self.class.post('/sessions', body: { email: email, password: password } )
-  #   @auth_token = response['auth_token']
-  #   if @auth_token.nil?
-  #     puts "Sorry, invalid credentials."
-  #   end
-  # end
+end
