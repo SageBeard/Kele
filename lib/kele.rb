e4ad4fe5b8825e66d41fcbbe549a71dbf7722c9b
Sage Beard <sagek711@gmail.com>
2017-09-05 22:45:15 -0700

    submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -49,4 +49,12 @@ class Kele
     response = self.class.get("#{base_uri}/messages", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    options = {
+      body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id }
+    }
+    response = self.class.get("#{base_uri}/checkpoint_submissions", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
 end
